69f5dd8db50b98c53d32730c3129e11b620fed59
morozoleh <olegmoroz93@gmail.com>
2021-03-06 12:03:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.92195a2a2c7eb2be4efae7ccec710dbc.js"
 );
 
 self.addEventListener('message', (event) => {
